39ff853e570ff9125524aeeb2c5ba57d99551952
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-03-21 14:28:57 +0900

    Added json example view

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # Rest Api
-1. Create
-2. Retrieve
+1. Create  
+2. Retrieve  -- List
 3. Update
 4. Delete
 
+# Method
+1. POST
+2. GET
+3. PUT (or Patch)
+4. DELETE
 
--- updates/views.py
+++ updates/views.py
@@ -1,3 +1,24 @@
+import json
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Update
+
+# def detail_view(request):
+    # return render(reqiest, template, {}) # return JSON data -> JS Object Notion
+    # return HttpResponse(get_template().render({}))
+
+
+def json_example_view(request):
+    '''
+    URL -- for a REST API
+    GET -- Retrieve
+    '''
+
+    data = {
+        "count": 1000,
+        "content": "Some new content",
+    }
+    json_data = json.dumps(data)
+    # return JsonResponse(data)
+    return HttpResponse(json_data, content_type='application/json')
