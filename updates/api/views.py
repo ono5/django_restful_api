cd691fdacc58b82f4c84e48c78b9ad1d52337617
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-03-24 06:51:50 +0900

    Practical API Module

---
+++ updates/api/views.py
@@ -0,0 +1,37 @@
+from updates.models import Update as UpdateModel
+
+
+# Creating, Updating, Deleting, Retrieving (1) -- Update Model
+
+class UpdateModelDetailAPIView():
+    def get(self, request, *args, **kwargs):
+        return # json # httpresponse
+
+    def post(self, request, *args, **kwargs):
+
+        return # json
+
+    def put(self, request, *args, **kwargs):
+        return  # json
+
+    def delete(self, request, *args, **kwargs):
+        return  # json
+
+class UpdateModeChangeAPIView():
+
+    def put(self, request, *args, **kwargs):
+        return  # json
+
+class UpdateModelListAPIView():
+    def get(self, request, *args, **kwargs):
+        return # json # httpresponse
+
+    def post(self, request, *args, **kwargs):
+
+        return # json
+
+    def put(self, request, *args, **kwargs):
+        return  # json
+
+    def delete(self, request, *args, **kwargs):
+        return  # json
