da4abc4339cd72dfaef1e4c8fb63049d116bc39e
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-03-25 22:47:41 +0900

    Creating the ConnectionPool class

--- postgres/database.py
+++ postgres/database.py
@@ -1,20 +1,23 @@
 from psycopg2 import pool
 
-
-# def connect():
-#     return psycopg2.connect(user='postgres',
-#                             password='1234',
-#                             database='learning',
-#                             host='localhost',
-#                             port=5433)
-
 # コネクションプールは、アプリケーションからのリクエストごとにデータベースへ接続するのではなく、
 # アプリケーション側である程度、接続を保持する仕組み
-connection_pool = pool.SimpleConnectionPool(1,
-                                            10,
-                                            database='learning',
-                                            user='postgres',
-                                            password="1234",
-                                            host="localhost",
-                                            port=5433)
 
+
+class ConnectionPool( object ):
+    def __init__(self):
+        self.connection_pool = pool.SimpleConnectionPool(1,
+                                                         1,
+                                                         database='learning',
+                                                         user='postgres',
+                                                         password="1234",
+                                                         host="localhost",
+                                                         port=5433 )
+
+    # with 文に入った時に呼ばれる
+    def __enter__(self):
+        return self.connection_pool.getconn()
+
+    # with 文を抜ける時に呼ばれる
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
--- postgres/user.py
+++ postgres/user.py
@@ -1,4 +1,4 @@
-from database import connection_pool
+from database import ConnectionPool
 
 
 class User(object):
@@ -12,18 +12,16 @@ class User(object):
         return "<User {}>".format(self.email)
 
     def save_to_db(self):
-        connection = connection_pool.getconn()
-        with connection.cursor() as cursor:
-            cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s, %s, %s)',
-                           (self.email, self.first_name, self.last_name))
-        connection_pool.putconn(connection)
+        with ConnectionPool() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('INSERT INTO users (email, first_name, last_name) VALUES (%s, %s, %s)',
+                               (self.email, self.first_name, self.last_name))
 
     @classmethod
     def load_from_db_by_email(cls, email):
-        connection = connection_pool.getconn()
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
-            user_data = cursor.fetchone()
-            print(user_data )
-            return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
+        with ConnectionPool() as connection:
+            with connection.cursor() as cursor:
+                cursor.execute("SELECT * FROM users WHERE email=%s", (email,))
+                user_data = cursor.fetchone()
+                return cls(email=user_data[1], first_name=user_data[2], last_name=user_data[3], id=user_data[0])
 
