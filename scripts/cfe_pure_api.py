2a189b3c40b5a782ea7e200225ad2029dc69bad0
ono5 <kuroneko_head_lord@yahoo.co.jp>
2019-03-30 06:19:44 +0900

    Added Update & Delete

--- scripts/cfe_pure_api.py
+++ scripts/cfe_pure_api.py
@@ -28,7 +28,7 @@ def create_update():
         'user': 1,
         'content': 'Another more cool content'
     }
-    r = requests.post(BASE_URL + ENDPOINT + "1/", data=new_data)
+    r = requests.post(BASE_URL + ENDPOINT + "1/", data=json.dumps(new_data))
     print(r.headers)
     print(r.status_code)
     if r.status_code == requests.codes.ok:
@@ -40,4 +40,42 @@ def create_update():
 # print(get_list())
 
 # get_list()
-print(create_update())
+# print(create_update())
+
+def do_obj_update():
+    new_data = {
+        'content': 'New obj data'
+    }
+
+    r = requests.put(BASE_URL + ENDPOINT + "1/", data=json.dumps(new_data))
+
+    # new_data = {
+    #     'user': 1,
+    #     'content': 'Another more cool content'
+    # }
+    # r = requests.post(BASE_URL + ENDPOINT, data=new_data)
+    print(r.status_code)
+    if r.status_code == requests.codes.ok:
+        # print(r.json())
+        return r.json()
+    return r.text
+
+# print(do_obj_update())
+
+def do_obj_delete():
+    new_data = {
+        'content': 'New obj data'
+    }
+
+    r = requests.delete(BASE_URL + ENDPOINT + "1/")
+
+    # new_data = {
+    #     'user': 1,
+    #     'content': 'Another more cool content'
+    # }
+    # r = requests.post(BASE_URL + ENDPOINT, data=new_data)
+    print(r.status_code)
+    if r.status_code == requests.codes.ok:
+        # print(r.json())
+        return r.json()
+    return r.text
